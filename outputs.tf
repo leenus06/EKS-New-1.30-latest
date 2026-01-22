d8f3fd5680109093704797bef7916ba493bedc63
leenus06 <146060227+leenus06@users.noreply.github.com>
2026-01-23 00:14:27 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,50 +1,23 @@
 output "region" {
-  description = "AWS region"
-  value       = var.region
+  value = var.region
 }
 
 output "cluster_name" {
-  description = "EKS cluster name"
-  value       = module.eks.cluster_name
-}
-
-output "cluster_arn" {
-  description = "Cluster ARN"
-  value       = module.eks.cluster_arn
+  value = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
-  description = "API endpoint"
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  description = "Cluster SG"
-  value       = module.eks.cluster_security_group_id
-}
-
-output "node_security_group_id" {
-  description = "Node SG"
-  value       = module.eks.node_security_group_id
+  value = module.eks.cluster_endpoint
 }
 
 output "vpc_id" {
-  description = "VPC ID"
-  value       = module.vpc.vpc_id
+  value = module.vpc.vpc_id
 }
 
 output "private_subnets" {
-  description = "Private subnet IDs"
-  value       = module.vpc.private_subnets
-}
-
-output "public_subnets" {
-  description = "Public subnet IDs"
-  value       = module.vpc.public_subnets
+  value = module.vpc.private_subnets
 }
 
 output "kubeconfig_cmd" {
-  description = "Command to set kubeconfig"
-  value       = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${var.region}"
+  value = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${var.region}"
 }
-
